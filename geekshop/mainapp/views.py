804cb1461384278391a141c2ebeb6a1b49c7cf7a
Andrei Safonov <klim1286@gmail.com>
2022-01-19 23:58:11 +0300

    lesson2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,12 +1,31 @@
+from turtle import title
 from django.shortcuts import render
+from django.utils import timezone
 
+MENU_MAIN_LINKS = [
+    {'url': 'main', 'name': 'Домой'},
+    {'url': 'products', 'name': 'Продукты'},
+    {'url': 'contact', 'name': 'Контакты'},
+]
 def index(request):
-    return render(request, 'mainapp\index.html')
+    return render(request, 'mainapp\index.html', context={
+        'menu_main_links': MENU_MAIN_LINKS,
+        'title': 'Главная',
+        'date': timezone.now,
+    })
 
 
 def contact(request):
-    return render(request, 'mainapp\contact.html')
+    return render(request, 'mainapp\contact.html',context={
+        'menu_main_links': MENU_MAIN_LINKS,
+        'title': 'Контакты',
+        'date': timezone.now,
+    })
 
 
 def products(request):
-    return render(request, 'mainapp\products.html')
+    return render(request, 'mainapp\products.html',context={
+        'menu_main_links': MENU_MAIN_LINKS,
+        'title': 'Продукты',
+        'date': timezone.now,
+    })
