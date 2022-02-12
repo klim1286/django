2d7d91295027adfd7841d3f62bc82b59c3665ad0
Andrei Safonov <klim1286@gmail.com>
2022-02-12 16:28:03 +0300

    lesson5

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -10,8 +10,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", mainapp.index, name="main"),
     path("contact/", mainapp.contact, name="contact"),
-    path("products/", mainapp.products, name="products"),
+    path("products/", include("mainapp.urls", namespace="products")),
     path("auth/", include("authapp.urls", namespace="auth")),
+    path("basket/", include("basketapp.urls", namespace="basket")),
 ]
 
 if settings.DEBUG:
--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,13 +1,11 @@
-from turtle import title
-from unicodedata import category
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Product, ProductCategory
 
 MAIN_MENU_LINKS = [
-    {"url": "main", "name": "Домой"},
-    {"url": "products", "name": "Продукты"},
-    {"url": "contact", "name": "Контакты"},
+    {"url": "main", "active":["main"], "name": "Домой"},
+    {"url": "products:all","active":["products:all", "products:category"], "name": "Продукты"},
+    {"url": "contact", "active":["contact"], "name": "Контакты"},
 ]
 
 
@@ -39,12 +37,32 @@ def contact(request):
 
 def products(request):
     catigories = ProductCategory.objects.all()
+    products = Product.objects.all()[:4]
     return render(
         request,
         "mainapp\products.html",
         context={
             "main_menu_links": MAIN_MENU_LINKS,
             "title": "Продукты",
+            "products": products,
+            "date": timezone.now,
+            "catigories": catigories,
+        },
+    )
+
+
+def category(request, pk):
+    catigories = ProductCategory.objects.all()
+    category = get_object_or_404(ProductCategory, pk=pk)
+    #category = ProductCategory.objects.get(pk=pk)
+    products = Product.objects.filter(category=category)
+    return render(
+        request,
+        "mainapp\products.html",
+        context={
+            "main_menu_links": MAIN_MENU_LINKS,
+            "title": "Продукты",
+            "products": products,
             "date": timezone.now,
             "catigories": catigories,
         },
