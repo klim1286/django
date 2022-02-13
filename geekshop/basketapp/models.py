8ec645a77ba2a0f5f366e23f735b0e1d989d60e1
Andrei Safonov <klim1286@gmail.com>
2022-02-13 13:15:14 +0300

    lesson6

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -2,13 +2,33 @@ from django.db import models
 from django.conf import settings
 from mainapp.models import Product
 
+
+class BasketManadger(models.Manager):
+    def total_quantity(self):
+        basket_items = self.all()
+        return sum(item.quantity for item in basket_items)
+
+    def total_cost(self):
+        basket_items = self.all()
+        return sum(item.cost for item in basket_items)
+
+
 class Basket(models.Model):
     class Meta:
-        unique_together = ['user', 'product']
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
+        unique_together = ["user", "product"]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="basket"
+    )
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
-    add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+    quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
+    add_datetime = models.DateTimeField(verbose_name="время", auto_now_add=True)
+
+    objects = BasketManadger()
+
+    @property
+    def cost(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
-        return f'{self.product.name} - {self.quantity} шт.'
+        return f"{self.product.name} - {self.quantity} шт."
--- geekshop/mainapp/urls.py
+++ geekshop/mainapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 
-app_name = 'mainapp'
+app_name = "mainapp"
 
 urlpatterns = [
-    path('', views.products, name='all'),
-    path('<int:pk>', views.category, name='category'),
-
+    path("", views.products, name="all"),
+    path("<int:pk>", views.category, name="category"),
+    path("product/<int:product_id>", views.product, name="product"),
 ]
